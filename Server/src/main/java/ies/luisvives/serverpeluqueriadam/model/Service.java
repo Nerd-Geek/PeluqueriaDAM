acbab2cafe47cfcb05a6151971b0b67eea86d3ee
Sergio Perez <sps169@outlook.es>
2022-02-10 18:08:52 +0100

    Server DAO created

--- Server/src/main/java/ies/luisvives/serverpeluqueriadam/model/Service.java
+++ Server/src/main/java/ies/luisvives/serverpeluqueriadam/model/Service.java
@@ -1,2 +1,65 @@
-package ies.luisvives.serverpeluqueriadam.model;public class Servicio {
+package ies.luisvives.serverpeluqueriadam.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "services")
+public class Service {
+    private String id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stock;
+
+    @Id
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Column(unique = true)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
 }
