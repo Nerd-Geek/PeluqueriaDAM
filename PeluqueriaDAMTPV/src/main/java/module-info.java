83da1b604c924d7b98af3138302baed835050889
Sergio <sps169@outlook.es>
2022-02-17 18:08:33 +0100

    TPV project initialization

---
+++ PeluqueriaDAMTPV/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ies.luisvives.peluqueriadamtpv {
+	requires javafx.controls;
+	requires javafx.fxml;
+
+
+	opens ies.luisvives.peluqueriadamtpv to javafx.fxml;
+	exports ies.luisvives.peluqueriadamtpv;
+}
