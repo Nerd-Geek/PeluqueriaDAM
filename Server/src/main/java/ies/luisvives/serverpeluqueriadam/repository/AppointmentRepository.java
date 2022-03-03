17a10ddf471020135aeb761917f22da28356d16f
Sergio Perez <sps169@outlook.es>
2022-03-03 18:52:13 +0100

    implemented appointment findAll updates

--- Server/src/main/java/ies/luisvives/serverpeluqueriadam/repository/AppointmentRepository.java
+++ Server/src/main/java/ies/luisvives/serverpeluqueriadam/repository/AppointmentRepository.java
@@ -6,13 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface AppointmentRepository extends JpaRepository<Appointment,String> {
-    List<Appointment> findByDate(Date date);
     //Page<Appointment> findByMonth(Pageable pageable, Date date);
 
-    List<Appointment> findByUser_UsernameContains(Optional<String> username);
+
 }
