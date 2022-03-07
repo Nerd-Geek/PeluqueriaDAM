0689ce829c8a9d9991b9bf8ed5c167fd62ba822b
Carlos Moreno <carlos.morenoluengos@gmail.com>
2022-03-07 19:02:54 +0100

    Modify

--- Server/src/test/java/ies/luisvives/serverpeluqueriadam/controllers/login/LoginRestControllerIntegrationMVCTest.java
+++ Server/src/test/java/ies/luisvives/serverpeluqueriadam/controllers/login/LoginRestControllerIntegrationMVCTest.java
@@ -104,7 +104,6 @@ public class LoginRestControllerIntegrationMVCTest {
                                         "GvqxqbWuJutdF_cnA7ulkwYz-LrDpAFrsFd9MFSQCL7Ms87cqALVqXsV4z0cphRYg"))
                 .andReturn().getResponse();
 
-        // then
         var res = jsonLoginDTO.parseObject(response.getContentAsString());
 
         assertAll(
@@ -118,79 +117,7 @@ public class LoginRestControllerIntegrationMVCTest {
 
     @Test
     @Order(3)
-    void saveTest() throws Exception {
-        LoginUserDTO loginUserDTO = LoginUserDTO.builder()
-                .username(user.getUsername())
-                .password(user.getPassword())
-                .build();
-        LoginDTO loginDTO = LoginDTO.builder()
-                .id(login.getId())
-                .user(loginUserDTO)
-                .instance(login.getInstant())
-                .token(login.getToken())
-                .build();
-
-
-        var response = mockMvc.perform(MockMvcRequestBuilders.post("/rest/logins/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonCreateDTO.write(loginDTO).getJson())
-                .accept(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOi" +
-                        "JjMTMzNGQ1Ny0xMjBiLTQzN2ItYmFlZi1jZjViNWY2OGNjM2UiLCJpYXQiOjE2NDY1NzY2MzgsImV4cCI6MT" +
-                        "Y0NjY2MzAzOCwibmFtZSI6IkFkbWluIiwicm9sZXMiOiJVU0VSLCBBRE1JTiJ9.HIY1f7O_OWY9SDfnJHkgN" +
-                        "GvqxqbWuJutdF_cnA7ulkwYz-LrDpAFrsFd9MFSQCL7Ms87cqALVqXsV4z0cphRYg")).andReturn().getResponse();
-
-        var res = jsonCreateDTO.parseObject(response.getContentAsString());
-
-        assertAll(
-                () -> assertEquals(HttpStatus.OK.value(), response.getStatus()),
-                () -> assertEquals(res.getUser().getUsername(), login.getUser().getUsername()),
-                () -> assertEquals(res.getInstance(), login.getInstant()),
-                () -> assertEquals(res.getToken(), login.getToken()),
-                () -> assertEquals(res.getUser().getUsername(), loginDTO.getUser().getUsername()),
-                () -> assertEquals(res.getInstance(), loginDTO.getInstance()),
-                () -> assertEquals(res.getToken(), loginDTO.getToken())
-        );
-    }
-
-    @Test
-    @Order(4)
-    void updateTest() throws Exception {
-        LoginUserDTO loginUserDTO = LoginUserDTO.builder()
-                .username(user.getUsername())
-                .password(user.getPassword())
-                .build();
-        LoginDTO loginDTO = LoginDTO.builder()
-                .user(loginUserDTO)
-                .instance(login.getInstant())
-                .token(login.getToken())
-                .build();
-
-        var response = mockMvc.perform(put("/rest/logins/" + login.getId())
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonLoginDTO.write(loginDTO).getJson())
-                .accept(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOi" +
-                        "JjMTMzNGQ1Ny0xMjBiLTQzN2ItYmFlZi1jZjViNWY2OGNjM2UiLCJpYXQiOjE2NDY1NzY2MzgsImV4cCI6MT" +
-                        "Y0NjY2MzAzOCwibmFtZSI6IkFkbWluIiwicm9sZXMiOiJVU0VSLCBBRE1JTiJ9.HIY1f7O_OWY9SDfnJHkgN" +
-                        "GvqxqbWuJutdF_cnA7ulkwYz-LrDpAFrsFd9MFSQCL7Ms87cqALVqXsV4z0cphRYg")).andReturn().getResponse();
-
-        var res = jsonLoginDTO.parseObject(response.getContentAsString());
-
-        assertAll(
-                () -> assertEquals(HttpStatus.OK.value(), response.getStatus()),
-                () -> assertEquals(res.getUser().getUsername(), login.getUser().getUsername()),
-                () -> assertEquals(res.getInstance(), login.getInstant()),
-                () -> assertEquals(res.getToken(), login.getToken()),
-                () -> assertEquals(res.getUser().getUsername(), loginDTO.getUser().getUsername()),
-                () -> assertEquals(res.getInstance(), loginDTO.getInstance()),
-                () -> assertEquals(res.getToken(), loginDTO.getToken())
-        );
-    }
-
-    @Test
-    @Order(5)
-    public void deleteTest() throws Exception {
+    void deleteTest() throws Exception {
 
         var response = mockMvc.perform(MockMvcRequestBuilders.delete("/rest/logins/" + login.getId())
                 .contentType(MediaType.APPLICATION_JSON)
